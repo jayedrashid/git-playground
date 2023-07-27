b19744da83add21c4d440e5536af4e46533bfcf3
Jayed Rashid <user@users-MacBook-Air.local>
2023-07-27 12:09:44 +0600

    created brA with heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import CountDown from './CountDown'
 export default function Hero() {
   return (
     <div className="w-full h-screen mt-[100px] ml-2">
+      <h1>I changed something in BranchA</h1>
        <CountDown seconds={10} />
     </div>
   )
